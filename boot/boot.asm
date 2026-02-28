7c94f6b8895583163971c425180c8bcd78a255d4
Enyytime <rogeliokennya@gmail.com>
2026-02-28 16:25:28 +1000

    change to write on video memory instead with BIOS

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,12 +1,28 @@
 [bits 16]
 [org 0x7c00]
 
+	jmp 0:start
+
 start:
-    mov ah, 0x0e      ; BIOS teletype function
-    mov al, '!'       ; Character to print
-    int 0x10          ; Print character
-    cli               ; Disable interrupts
-    hlt               ; Halt CPU
+	; set register to 0
+	xor ax, ax
+	mov ds, ax ; this sets the data segment to 0, so it starts at the bottom of the memory (0x7c00)
+	mov es, ax
+
+	; point to video's memory address (VGA)
+	mov ax, 0xb800
+	mov es, ax
+
+	; write H
+	mov byte [es:0x0000], 'H'
+	mov byte [es:0x0001], 0x4f ; white on black
+	mov byte [es:0x0002], 'E'
+	mov byte [es:0x0003], 0x4f
+hang:
+	cli
+	hlt
+	jmp hang
 
 times 510-($-$$) db 0
-dw 0xAA55             ; Boot signature
+dw 0xAA55
+
