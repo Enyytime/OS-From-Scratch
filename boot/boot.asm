df33218d86fb2524a73b50abd0e34eb984089742
Enyytime <kennectic@LAPTOP-30NNQS4E.localdomain>
2026-03-11 09:18:41 +1000

    Syncing boot and kernel files

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,28 +1,51 @@
-[bits 16]
+; Identical to lesson 13's boot sector, but the %included files have new paths
 [org 0x7c00]
+KERNEL_OFFSET equ 0x1000 ; The same one we used when linking the kernel
 
-	jmp 0:start
+    mov [BOOT_DRIVE], dl ; Remember that the BIOS sets us the boot drive in 'dl' on boot
+    mov bp, 0x9000
+    mov sp, bp
 
-start:
-	; set register to 0
-	xor ax, ax
-	mov ds, ax ; this sets the data segment to 0, so it starts at the bottom of the memory (0x7c00)
-	mov es, ax
+    mov bx, MSG_REAL_MODE 
+    call print
+    call print_nl
 
-	; point to video's memory address (VGA)
-	mov ax, 0xb800
-	mov es, ax
+    call load_kernel ; read the kernel from disk
+    call switch_to_pm ; disable interrupts, load GDT,  etc. Finally jumps to 'BEGIN_PM'
+    jmp $ ; Never executed
 
-	; write H
-	mov byte [es:0x0000], 'H'
-	mov byte [es:0x0001], 0x4f ; white on black
-	mov byte [es:0x0002], 'E'
-	mov byte [es:0x0003], 0x4f
-hang:
-	cli
-	hlt
-	jmp hang
+%include "boot/print.asm"
+%include "boot/print_hex.asm"
+%include "boot/disk.asm"
+%include "boot/gdt.asm"
+%include "boot/32bit_print.asm"
+%include "boot/switch_pm.asm"
 
-times 510-($-$$) db 0
-dw 0xAA55
+[bits 16]
+load_kernel:
+    mov bx, MSG_LOAD_KERNEL
+    call print
+    call print_nl
 
+    mov bx, KERNEL_OFFSET ; Read from disk and store in 0x1000
+    mov dh, 16 ; Our future kernel will be larger, make this big
+    mov dl, [BOOT_DRIVE]
+    call disk_load
+    ret
+
+[bits 32]
+BEGIN_PM:
+    mov ebx, MSG_PROT_MODE
+    call print_string_pm
+    call KERNEL_OFFSET ; Give control to the kernel
+    jmp $ ; Stay here when the kernel returns control to us (if ever)
+
+
+BOOT_DRIVE db 0 ; It is a good idea to store it in memory because 'dl' may get overwritten
+MSG_REAL_MODE db "Started in 16-bit Real Mode", 0
+MSG_PROT_MODE db "Landed in 32-bit Protected Mode", 0
+MSG_LOAD_KERNEL db "Loading kernel into memory", 0
+
+; padding
+times 510 - ($-$$) db 0
+dw 0xaa55
